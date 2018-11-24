c11062eed336bcbf0fcfc1b2de4b0e8f59c97498
Pavol Almasi <pavolalmasi@gmail.com>
2018-11-24 18:57:18 -0500

    Completed the module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -20,7 +20,9 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }                
             else
                 app.UseExceptionHandler("/Home/Error");
 
