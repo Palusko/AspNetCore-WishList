1d18c8046209d5411e0b47271f1ff0fa63895ee6
Pavol Almasi <pavolalmasi@gmail.com>
2018-11-24 18:48:09 -0500

    complete project

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        //index action
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //error action
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
